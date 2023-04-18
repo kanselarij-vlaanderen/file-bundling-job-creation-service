34ea586429a1c3f937dc2ded720183f891bf38ed
Sven Valenberghs <sven.valenberghs@faros.be>
2023-04-18 15:11:18 +0200

    Change prefix of newly added code with prefix besluitvorming

--- queries/agenda.js
+++ queries/agenda.js
@@ -34,7 +34,7 @@ const fetchFilesFromAgendaByMandatees = async (agendaId, mandateeIds) => {
   const queryString = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
   PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-  PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+  PREFIX besluitvorming: <https://data.vlaanderen.be/ns/besluitvorming#>
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX dbpedia: <http://dbpedia.org/ontology/>
   PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
@@ -76,7 +76,7 @@ const fetchDecisionsByMandatees = async (agendaId, mandateeIds) => {
   const queryString = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
   PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-  PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+  PREFIX besluitvorming: <https://data.vlaanderen.be/ns/besluitvorming#>
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX dbpedia: <http://dbpedia.org/ontology/>
   PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
@@ -118,7 +118,7 @@ const fetchDecisionsFromAgenda = async (agendaId) => {
   const queryString = `
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
   PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
-  PREFIX besluitvorming: <http://data.vlaanderen.be/ns/besluitvorming#>
+  PREFIX besluitvorming: <https://data.vlaanderen.be/ns/besluitvorming#>
   PREFIX dct: <http://purl.org/dc/terms/>
   PREFIX dbpedia: <http://dbpedia.org/ontology/>
   PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
